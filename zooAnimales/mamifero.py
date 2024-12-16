8d5669cbe0698bc3a52a2eadbcec867a8f8677fd
Tomas Ospina Gaviria <tospinag@unal.edu.co>
2024-12-16 15:55:12 -0500

    Some changed related on file-based organization were made

---
+++ zooAnimales/mamifero.py
@@ -0,0 +1,26 @@
+from zooAnimales.animal import Animal
+
+class Mamifero(Animal):
+    listado = []
+    caballos = 0
+    leones = 0
+
+    def __init__(self, nombre=None, edad=None, habitat=None, genero=None, pelaje=None, patas=None):
+        super().__init__(nombre, edad, habitat, genero)
+        self.pelaje = pelaje
+        self.patas = patas
+        Mamifero.listado.append(self)
+
+    @staticmethod
+    def cantidad_mamiferos():
+        return len(Mamifero.listado)
+
+    @staticmethod
+    def crear_caballo(nombre, edad, genero):
+        Mamifero.caballos += 1
+        return Mamifero(nombre, edad, "pradera", genero, True, 4)
+
+    @staticmethod
+    def crear_leon(nombre, edad, genero):
+        Mamifero.leones += 1
+        return Mamifero(nombre, edad, "selva", genero, True, 4)
